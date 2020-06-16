e208e91d5c5bc29c98cfd3387de8882cb5c0d72e
Marylange <marylangess@gmail.com>
2020-06-16 16:52:35 -0300

    Atualizando o map

--- HOF - manipulação e criação de Arrays/MAP-transformar objeto de uma lista.js
+++ HOF - manipulação e criação de Arrays/MAP-transformar objeto de uma lista.js
@@ -1,42 +1,45 @@
-//Transformar elementos de uma lista em outros elementos
+//Transforma elementos de uma lista em outros elementos
 //ex: transformar lista de strings em lista de objetos
 
 const list = ["item1", "item2", "item3", "item4", "item5"];
 
 //Caso com for
 
-function listaComFor() {
+const listaComFor = (lista) => {
   const objetosTransformados = []
-  for (let x = 0; x < list.length; x++) {
+  for (let x = 0; x < lista.length; x++) {
     let obj = {};
-    obj.nome = list[x];
+    obj.nome = lista[x];
     objetosTransformados.push(obj);
   }
 
   return objetosTransformados;
 }
-
+console.log(listaComFor(list));
 
 // Transformar item em {nome : item}
 //["item1", "item2", "item3", "item4", "item5"] => [{"nome": "item1"}, {"nome": "item2"}, {"nome": "item3"}, {"nome": "item4"}, {"nome": "item5"}]
-function listComMap() {
-  // a variavel item usana na arrow function pode ter qualquer nome
-  // EX: list.find(xuxu => xuxu => {})
-  // O map retorna um novo elemento para cada elemento da lista
-  // a arrow function tem a finalidade de verificar se cada item é igual a "item2"
-  const objetosTransformados = list.map(item => {
+
+const listComMap = (lista) => {
+  const objetosTransformados = lista.map(item => {
     let obj = {};
     obj.nome = item;
     return obj;
   });
   return objetosTransformados;
 }
+console.log(listComMap(list));
 
-// Verificando a quantidade de letras nos elementos e retornando um array com essas quantidades.
-list.map(item => item.length); //[5, 5, 5, 5, 5]
-list.map(item => item + " " + "Que item"); //["item1 Que item", "item2 Que item", "item3 Que item", "item4 Que item", "item5 Que item"]
+/*
+a variavel item usada na arrow function pode ter qualquer nome
+EX: list.find(xuxu => xuxu => {})
 
+O MAP retorna um novo elemento para cada elemento da lista
+*/
 
+/*
+  OBS: O [].map - mapeamento/transformação - Para cada elemento da lista será feito
+  uma transformação para um novo elemento que será incorporado numa lista.
 
-//OBS: O [].map - mapeamento/transformação - Para cada elemento da lista será feito uma transformação para um novo elemento que será incorporado numa lista.
-// mapeamento transforma um elemento em outro, cria um elemento com base em outro.
+  O MAP transforma um elemento em outro, cria um elemento com base em outro.
+*/
--- HOF - manipulação e criação de Arrays/SORT-Ordena o array de acordo com um criterio.js
+++ HOF - manipulação e criação de Arrays/SORT-Ordena o array de acordo com um criterio.js
@@ -31,19 +31,19 @@ console.log(food);
 */
 
 const points = [40, 100, 1, 5, 25, 10];
-points.sort(function(a, b){ return a - b });
+points.sort(function (a, b) { return a - b });
 console.log(points); // [1, 5, 10, 25, 40, 100]
 
 
 /*
     O acontece nesta condição:
 
-    A lógica é a seguinte: a função recebe, da sort, todos os 
-    elementos do array, em pares, e vai 
-    comparando-os. O formato é meuArray.sort((elemento1, elemento2) => { logica da função }). Ou 
-    seja: para o array [1, 2, 3, 4], a função receberá (1, 2), (1, 3), …, (3, 4) como parâmetros 
-    e ordenará o array com base em seu resultado. Se a operação de 
-    elemento1 com elemento2 der resultado negativo, elemento1 vai para trás. Caso contrário, vai 
-    para frente, para, de forma decrescente, só inverter 
-    elemento1 - elemento2 para elemento2 - elemento1. 
+    A lógica é a seguinte: a função recebe, da sort, todos os
+    elementos do array, em pares, e vai
+    comparando-os. O formato é meuArray.sort((elemento1, elemento2) => { logica da função }). Ou
+    seja: para o array [1, 2, 3, 4], a função receberá (1, 2), (1, 3), …, (3, 4) como parâmetros
+    e ordenará o array com base em seu resultado. Se a operação de
+    elemento1 com elemento2 der resultado negativo, elemento1 vai para trás. Caso contrário, vai
+    para frente, para, de forma decrescente, só inverter
+    elemento1 - elemento2 para elemento2 - elemento1.
 */
