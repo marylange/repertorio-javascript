15a613cbd13ec0018f8a3dab7f59911c2215049d
Marylange <marylangess@gmail.com>
2020-08-17 13:37:34 -0300

    atualizando reper com instalação react redux e jest

---
+++ intalação-do-React-com-Redux-e-teste-com-Jest/install.js
@@ -0,0 +1,35 @@
+/*
+
+
+Primeiro, vamos começar um novo app em React. Crie um diretório e use o comando:
+
+    npx create-react-app doguinhos-app
+
+
+Depois de terminar, vamos acessar o diretório do nosso novo app e instalar as dependências necessárias:
+
+    cd doguinhos-app
+    npm i redux react-redux
+
+
+Instalar a pasta node-modules para testes
+
+    npm install --save-dev jest
+
+instalar o pacote thunk para códigos assincronos
+
+    npm i redux-thunk
+
+    OBS: Lembrando de colocar os devidos imports
+
+    // src/store/index.js
+    import { createStore, applyMiddleware } from 'redux';
+    import ReduxThunk from 'redux-thunk';
+    .
+    .
+    .
+
+    const store = createStore(reducer, applyMiddleware(ReduxThunk));
+
+
+*/
